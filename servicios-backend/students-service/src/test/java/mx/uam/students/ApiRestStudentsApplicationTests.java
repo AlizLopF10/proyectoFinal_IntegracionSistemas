e62c67f0e4275dd337a8ebbb9117ee531e9dc592
AlizLopF10 <alicia.lopez@cua.uam.mx>
2025-04-23 16:48:18 -0600

    Add files via upload

---
+++ servicios-backend/students-service/src/test/java/mx/uam/students/ApiRestStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
